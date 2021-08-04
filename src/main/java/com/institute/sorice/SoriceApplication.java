d04bd0bdbae18909fc05063f88b8cf64bd49bc5b
kevin-vakayil <vakayilkevin@gmail.com>
2021-08-04 11:46:56 +0530

    new

---
+++ src/main/java/com/institute/sorice/SoriceApplication.java
@@ -0,0 +1,13 @@
+package com.institute.sorice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/institute/sorice/SoriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institute.sorice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
